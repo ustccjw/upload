12fc413cecc17113eb9348b404598b7c4cef1313
ustccjw <317713370@qq.com>
2015-02-10 18:20:44 +0800

    add upload

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -9,14 +9,29 @@ var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 var buffer = require('vinyl-buffer')
 
-gulp.task('browserify', function () {
+gulp.task('image-upload', function () {
     return browserify([
         './client/image_upload.js',
     ], {
         standalone: 'imageUpload'
     }).
     bundle().
-    pipe(source('bundle.min.js')).
+    pipe(source('image_upload.min.js')).
+    pipe(buffer()).
+    pipe(sourcemaps.init({loadMaps: true})).
+    pipe(uglify()).
+    pipe(sourcemaps.write('./')).
+    pipe(gulp.dest('./public/javascript'))
+})
+
+gulp.task('upload', function () {
+    return browserify([
+        './client/lib/upload.js',
+    ], {
+        standalone: 'Upload'
+    }).
+    bundle().
+    pipe(source('upload.min.js')).
     pipe(buffer()).
     pipe(sourcemaps.init({loadMaps: true})).
     pipe(uglify()).
@@ -29,4 +44,4 @@ gulp.task('copy', function () {
         pipe(gulp.dest('../'))
 })
 
-gulp.task('default', ['browserify', 'copy'])
+gulp.task('default', ['image-upload', 'upload', 'copy'])
