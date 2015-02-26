618671984bccc3de1d1377c205fee37714d55a04
ustccjw <317713370@qq.com>
2015-02-26 14:30:17 +0800

    fix ios window.location.origin read-only

--- demo/client/lib/compress.js
+++ demo/client/lib/compress.js
@@ -11,60 +11,60 @@ ES6Promise.polyfill()
  */
 function compress(file, options) {
 
-    // do not need to compress
-    if (options === false) {
-        return Promise.resolve(file)
-    }
+	// do not need to compress
+	if (options === false) {
+		return Promise.resolve(file)
+	}
 
-    // check File API support
-    if (!window.File || !window.FileReader || !window.URL) {
-        return Promise.reject(new Error('File API not support'))
-    }
+	// check File API support
+	if (!window.File || !window.FileReader || !window.URL) {
+		return Promise.reject(new Error('File API not support'))
+	}
 
-    // check file param
-    if (!(file instanceof File)) {
-        return Promise.reject(new Error('file invalid'))
-    }
+	// check file param
+	if (!(file instanceof File)) {
+		return Promise.reject(new Error('file invalid'))
+	}
 
-    // can not compress
-    if (!(/image/i).test(file.type)) {
-        return Promise.reject(new Error('file is not image'))
-    }
-    if (typeof options !== 'object') {
-        options = {}
-    }
-    return new Promise(function (resolve, reject) {
+	// can not compress
+	if (!(/image/i).test(file.type)) {
+		return Promise.reject(new Error('file is not image'))
+	}
+	if (typeof options !== 'object') {
+		options = {}
+	}
+	return new Promise(function (resolve, reject) {
 
-        // read the files
-        var reader = new FileReader()
-        reader.readAsArrayBuffer(file)
-        reader.onload = function (event) {
+		// read the files
+		var reader = new FileReader()
+		reader.readAsArrayBuffer(file)
+		reader.onload = function (event) {
 
-            // blob stuff
-            try {
-                var blob = new Blob([event.target.result])
-                window.URL = window.URL || window.webkitURL
-                var blobURL = window.URL.createObjectURL(blob)
+			// blob stuff
+			try {
+				var blob = new Blob([event.target.result])
+				window.URL = window.URL || window.webkitURL
+				var blobURL = window.URL.createObjectURL(blob)
 
-                // helper Image object
-                var image = new Image()
-                image.src = blobURL
-                image.onload = function() {
+				// helper Image object
+				var image = new Image()
+				image.src = blobURL
+				image.onload = function() {
 
-                    // send it to canvas
-                    try {
-                        var dataURI = resize(image, file.type, options)
-                        var blob = dataURItoBlob(dataURI)
-                        resolve(blob)
-                    } catch (err) {
-                        reject(err)
-                    }
-                }
-            } catch (err) {
-                reject(err)
-            }
-        }
-    })
+					// send it to canvas
+					try {
+						var dataURI = resize(image, file.type, options)
+						var blob = dataURItoBlob(dataURI)
+						resolve(blob)
+					} catch (err) {
+						reject(err)
+					}
+				}
+			} catch (err) {
+				reject(err)
+			}
+		}
+	})
 }
 
 /**
@@ -75,32 +75,32 @@ function compress(file, options) {
  * @return {string}          base64/URLEncoded data
  */
 function resize(img, type, options) {
-    var width = img.width
-    var height = img.height
-    var max_width = options.max_width || width
-    var max_height = options.max_height || height
-    var quality = options.quality || null
+	var width = img.width
+	var height = img.height
+	var max_width = options.max_width || width
+	var max_height = options.max_height || height
+	var quality = options.quality || null
 
-    // calculate the width and height, constraining the proportions
-    if (width > height) {
-        if (width > max_width) {
-            height = Math.round(height *= max_width / width)
-            width = max_width
-        }
-    } else {
-        if (height > max_height) {
-            width = Math.round(width *= max_height / height)
-            height = max_height
-        }
-    }
+	// calculate the width and height, constraining the proportions
+	if (width > height) {
+		if (width > max_width) {
+			height = Math.round(height *= max_width / width)
+			width = max_width
+		}
+	} else {
+		if (height > max_height) {
+			width = Math.round(width *= max_height / height)
+			height = max_height
+		}
+	}
 
-    // resize the canvas and draw the image data into it
-    var canvas = document.createElement('canvas')
-    canvas.width = width
-    canvas.height = height
-    var ctx = canvas.getContext("2d")
-    ctx.drawImage(img, 0, 0, width, height)
-    return canvas.toDataURL(type, quality)
+	// resize the canvas and draw the image data into it
+	var canvas = document.createElement('canvas')
+	canvas.width = width
+	canvas.height = height
+	var ctx = canvas.getContext("2d")
+	ctx.drawImage(img, 0, 0, width, height)
+	return canvas.toDataURL(type, quality)
 }
 
 /**
@@ -109,23 +109,23 @@ function resize(img, type, options) {
  * @return {string}         raw binary data
  */
 function dataURItoBlob(dataURI) {
-    var byteString = ''
-    if (dataURI.split(',')[0].indexOf('base64') >= 0) {
-        byteString = atob(dataURI.split(',')[1])
-    }
-    else {
-        byteString = unescape(dataURI.split(',')[1])
-    }
+	var byteString = ''
+	if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+		byteString = atob(dataURI.split(',')[1])
+	}
+	else {
+		byteString = unescape(dataURI.split(',')[1])
+	}
 
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+	// separate out the mime component
+	var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
 
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length)
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i)
-    }
-    return new Blob([ia], {type: mimeString})
+	// write the bytes of the string to a typed array
+	var ia = new Uint8Array(byteString.length)
+	for (var i = 0; i < byteString.length; i++) {
+		ia[i] = byteString.charCodeAt(i)
+	}
+	return new Blob([ia], {type: mimeString})
 }
 
 module.exports = compress
--- image_upload.js
+++ image_upload.js
@@ -8,25 +8,25 @@ var vendorConfig = require('./lib/vendor_config')
  * @return {Promise}        promise resolve upload object, reject any error (mostly: server_config error: xxxx)
  */
 function imageUpload(vendor, options) {
-    if (typeof vendor === 'object') {
-        options = vendor
-        vendor = 'upyun'
-    }
-    vendor = vendor || 'upyun'
-    if (typeof options !== 'object') {
-        options = {}
-    }
-    return vendorConfig('image', vendor).then(function (config) {
-        $.extend(config, {
-            accept: 'image/*',
-            timeout: 10000,
-            compress: {
-                max_width: 300,
-                max_height: 300
-            }
-        }, options)
-        return new Upload(config)
-    })
+	if (typeof vendor === 'object') {
+		options = vendor
+		vendor = 'upyun'
+	}
+	vendor = vendor || 'upyun'
+	if (typeof options !== 'object') {
+		options = {}
+	}
+	return vendorConfig('image', vendor).then(function (config) {
+		$.extend(config, {
+			accept: 'image/*',
+			timeout: 10000,
+			compress: {
+				max_width: 300,
+				max_height: 300
+			}
+		}, options)
+		return new Upload(config)
+	})
 }
 
 /**
@@ -37,15 +37,15 @@ function imageUpload(vendor, options) {
  * @return {string}          Thumbnail url
  */
 function getThumbnailUrl(vendor, response, suffix) {
-    vendor = vendor || 'upyun'
-    var url = response.url || response.upload_ret.url
-    url = url.split('#')[0]
-    suffix = suffix ? ('_' + suffix) : '_sq'
-    if (vendor === 'upyun') {
-        return 'http://img' + (url.charCodeAt(1) % 3 + 4) + '.baixing.net' + url + suffix
-    } else if (vendor === 'qiniu') {
-        return 'http://img7.baixing.net/' + url + suffix
-    }
+	vendor = vendor || 'upyun'
+	var url = response.url || response.upload_ret.url
+	url = url.split('#')[0]
+	suffix = suffix ? ('_' + suffix) : '_sq'
+	if (vendor === 'upyun') {
+		return 'http://img' + (url.charCodeAt(1) % 3 + 4) + '.baixing.net' + url + suffix
+	} else if (vendor === 'qiniu') {
+		return 'http://img7.baixing.net/' + url + suffix
+	}
 }
 
 /**
@@ -55,52 +55,52 @@ function getThumbnailUrl(vendor, response, suffix) {
  * @return {string}          url
  */
 function getUrl(vendor, response) {
-    vendor = vendor || 'upyun'
-    var url = response.url || response.upload_ret.url
-    url = url.split('#')[0]
-    var suffix = ''
-    if (vendor === 'upyun' || vendor === 'upyun_im') {
-        suffix = '#up'
-    } else if (vendor === 'qiniu') {
-        suffix = '#qn'
-    }
-    return url + suffix
+	vendor = vendor || 'upyun'
+	var url = response.url || response.upload_ret.url
+	url = url.split('#')[0]
+	var suffix = ''
+	if (vendor === 'upyun' || vendor === 'upyun_im') {
+		suffix = '#up'
+	} else if (vendor === 'qiniu') {
+		suffix = '#qn'
+	}
+	return url + suffix
 }
 
 // image-upload init via data-API
 $(function () {
-    $('[data-image-upload]').each(function (index, element) {
-        var vendor = $(element).data('vendor')
-        var accept = $(element).data('accept')
-        var suffix = $(element).data('suffix')
-        imageUpload(vendor, {
-            trigger: $(element),
-            accept: accept,
-            success: function (response, uid) {
-                if (typeof response === 'string') {
-                    response = $.parseJSON(response)
-                }
-                if (response.code === 200 || (vendor === 'qiniu' && !response.error)) {
-                    var thumbnailUrl = getThumbnailUrl(vendor, response, suffix)
-                    var url = getUrl(vendor, response)
-                    $(element).trigger('imageUploadSuccess', [thumbnailUrl, url, uid])
-                } else {
-                    $(element).trigger('imageUploadError', [response.message || response.error, uid])
-                }
-            },
-            error: function (err, uid) {
-                $(element).trigger('imageUploadError', [err.message, uid])
-            },
-            progress: function (position, total, percent, uid) {
-                $(element).trigger('imageUploadProgress', [].slice.call(arguments, 0))
-            },
-            select: function (files, uids) {
-                $(element).trigger('imageUploadSelect', [].slice.call(arguments, 0))
-                this.submit()
-            }
-        })['catch'](function (err) {
-            console.log(err)
-        })
-    })
+	$('[data-image-upload]').each(function (index, element) {
+		var vendor = $(element).data('vendor')
+		var accept = $(element).data('accept')
+		var suffix = $(element).data('suffix')
+		imageUpload(vendor, {
+			trigger: $(element),
+			accept: accept,
+			success: function (response, uid) {
+				if (typeof response === 'string') {
+					response = $.parseJSON(response)
+				}
+				if (response.code === 200 || (vendor === 'qiniu' && !response.error)) {
+					var thumbnailUrl = getThumbnailUrl(vendor, response, suffix)
+					var url = getUrl(vendor, response)
+					$(element).trigger('imageUploadSuccess', [thumbnailUrl, url, uid])
+				} else {
+					$(element).trigger('imageUploadError', [response.message || response.error, uid])
+				}
+			},
+			error: function (err, uid) {
+				$(element).trigger('imageUploadError', [err.message, uid])
+			},
+			progress: function (position, total, percent, uid) {
+				$(element).trigger('imageUploadProgress', [].slice.call(arguments, 0))
+			},
+			select: function (files, uids) {
+				$(element).trigger('imageUploadSelect', [].slice.call(arguments, 0))
+				this.submit()
+			}
+		})['catch'](function (err) {
+			console.log(err)
+		})
+	})
 })
 
